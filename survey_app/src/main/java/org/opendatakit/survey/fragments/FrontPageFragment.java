df796ed45f652bf73ef89e0e2c3f496060314a49
Artur Fijał <afijal@soldevelo.com>
2017-03-23 13:17:09 +0100

    ODK-60: Fix crash on fresh install Seems like this is required

--- survey_app/src/main/java/org/opendatakit/survey/fragments/FrontPageFragment.java
+++ survey_app/src/main/java/org/opendatakit/survey/fragments/FrontPageFragment.java
@@ -120,7 +120,7 @@ public class FrontPageFragment extends Fragment implements Button.OnClickListene
         try {
             c = getActivity().getContentResolver().query(baseUri, null, null, null, null);
 
-            if ( c.moveToFirst() ) {
+            if ( c != null && c.moveToFirst() ) {
                 int idxTableId = c.getColumnIndex(FormsColumns.TABLE_ID);
                 int idxFormVersion = c.getColumnIndex(FormsColumns.FORM_VERSION);
                 do {
