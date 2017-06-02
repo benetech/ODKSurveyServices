2a96f24b0c8e1996344d16e4282bd4413ee674a3
Jakub Redlarski <jredlarski@soldevelo.com>
2017-06-02 15:15:46 +0200

    ODK-155: Added storing reporter name and ID and server URL textbox field
    Enabled saving settings in the properties singleton and retrieving them on settings fragment load.
    Added additional field for entering server URL by user for testing purposes. In the final version this should be loaded from the config file on app build. The same goes for the office ID.
    
    TODO:
    - get the application name from the activity
    - add checking if certain property exists before displaying its value

--- survey_app/src/main/java/org/opendatakit/survey/fragments/SettingsFragment.java
+++ survey_app/src/main/java/org/opendatakit/survey/fragments/SettingsFragment.java
@@ -2,108 +2,100 @@ package org.opendatakit.survey.fragments;
 
 import android.app.Fragment;
 import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import org.opendatakit.demoAndroidlibraryClasses.properties.CommonToolProperties;
+import org.opendatakit.demoAndroidlibraryClasses.properties.PropertiesSingleton;
 import org.opendatakit.survey.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link SettingsFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link SettingsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class SettingsFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+public class SettingsFragment extends Fragment implements View.OnClickListener {
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    private OnFragmentInteractionListener mListener;
-
-    public SettingsFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SettingsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static SettingsFragment newInstance(String param1, String param2) {
-        SettingsFragment fragment = new SettingsFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    private View view;
+    private PropertiesSingleton properties;
+    private EditText reporterNameEditText;
+    private EditText reporterIdEditText;
+    private EditText officeIdEditText;
+    private EditText syncServerUrlEditText;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+
+        view = inflater.inflate(R.layout.fragment_settings, container, false);
+
+        Button saveButton = (Button) view.findViewById(R.id.settings_save_button);
+        saveButton.setOnClickListener(this);
+
+        reporterNameEditText = (EditText) view.findViewById(R.id.reporter_name_edit_text);
+        reporterIdEditText = (EditText) view.findViewById(R.id.reporter_id_edit_text);
+        officeIdEditText = (EditText) view.findViewById(R.id.office_id_edit_text);
+        syncServerUrlEditText = (EditText) view.findViewById(R.id.sync_server_url_edit_text);
+
+        // Get saved properties singleton
+        //TODO: getAppName to get properties
+        properties = CommonToolProperties.get(this.getActivity().getApplicationContext(), "default");
+
+        //TODO: check if these values exist to prevent errors
+        reporterNameEditText.setText(properties.getProperty(CommonToolProperties.KEY_REPORTER_NAME));
+        reporterIdEditText.setText(properties.getProperty(CommonToolProperties.KEY_REPORTER_ID));
+        officeIdEditText.setText(properties.getProperty(CommonToolProperties.KEY_OFFICE_ID));
+        syncServerUrlEditText.setText(properties.getProperty(CommonToolProperties.KEY_SYNC_SERVER_URL));
+
+        // Enable automatic resizing of the fragment space when the keyboard is visible so it does
+        // not hide the editText fields underneath it
+        this.getActivity().getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
+
+        return view;
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.settings_save_button:
+                saveSettings();
+                break;
         }
     }
-//
-//    @Override
-//    public void onAttach(Context context) {
-//        super.onAttach(context);
-//        if (context instanceof OnFragmentInteractionListener) {
-//            mListener = (OnFragmentInteractionListener) context;
-//        } else {
-//            throw new RuntimeException(context.toString()
-//                    + " must implement OnFragmentInteractionListener");
-//        }
-//    }
-//
-//    @Override
-//    public void onDetach() {
-//        super.onDetach();
-//        mListener = null;
-//    }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
+    private void saveSettings() {
+
+        // Set properties which hold setting to be saved
+        properties.setProperty(CommonToolProperties.KEY_REPORTER_NAME, reporterNameEditText.getText().toString());
+        properties.setProperty(CommonToolProperties.KEY_REPORTER_ID, reporterIdEditText.getText().toString());
+        properties.setProperty(CommonToolProperties.KEY_OFFICE_ID, officeIdEditText.getText().toString());
+        properties.setProperty(CommonToolProperties.KEY_SYNC_SERVER_URL, syncServerUrlEditText.getText().toString());
+
+        // Save properties
+        try {
+            properties.writeProperties();
+
+            Toast.makeText(this.getActivity(),
+                    getString(R.string.save_settings_success),
+                    Toast.LENGTH_LONG).show();
+        } catch (Exception e) {
+            Toast.makeText(this.getActivity(),
+                    getString(R.string.save_settings_failure),
+                    Toast.LENGTH_LONG).show();
+        }
+
+        // Return to the previous view
+        getActivity().onBackPressed();
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
     }
 }
