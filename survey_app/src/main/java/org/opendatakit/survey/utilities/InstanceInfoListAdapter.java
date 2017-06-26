fc9d242075f4bbe9352dcf528d35264eb209013b
Jakub Redlarski <jredlarski@soldevelo.com>
2017-06-26 16:01:05 +0200

    ODK-164: Added connection with the server via SyncActivity
    It is possible to download form definitions by selecting the Check For Updates button and upload choosen completed form after selecting the cloud image on the In Progress Surveys list. However, both actions are launching the SyncActivity from the old Services app.
    TODO: remove the SyncActivity being launched in the middle of the process and call directly the sync service methods.

--- survey_app/src/main/java/org/opendatakit/survey/utilities/InstanceInfoListAdapter.java
+++ survey_app/src/main/java/org/opendatakit/survey/utilities/InstanceInfoListAdapter.java
@@ -14,25 +14,33 @@
 
 package org.opendatakit.survey.utilities;
 
+import android.content.ComponentName;
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
+import android.content.Intent;
 import android.graphics.Color;
+import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.*;
+
+import org.opendatakit.demoAndroidlibraryClasses.consts.IntentConsts;
+import org.opendatakit.demoAndroidlibraryClasses.utilities.ODKFileUtils;
 import org.opendatakit.survey.R;
-import org.opendatakit.survey.activities.MainMenuActivity;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Implementation of cursor adapter that displays instance info
  *
  */
 public class InstanceInfoListAdapter extends BaseAdapter {
+
+  private static final String TAG = InstanceInfoListAdapter.class.getSimpleName();
+
 
   private final Context mContext;
   private final int mLayout;
@@ -110,7 +118,7 @@ public class InstanceInfoListAdapter extends BaseAdapter {
           view = layoutInflater.inflate(mLayout, parent, false);
         }
 
-        InstanceInfo info = (InstanceInfo) mItems.get(position);
+        final InstanceInfo info = (InstanceInfo) mItems.get(position);
         TextView formBeneficiaryView = (TextView) view.findViewById(mBeneficiaryInformation);
         formBeneficiaryView.setText(info.beneficiaryInformation);
         PieGraph pg = (PieGraph) view.findViewById(R.id.graph);
@@ -121,6 +129,86 @@ public class InstanceInfoListAdapter extends BaseAdapter {
           cloud.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
               System.out.println("Cloud clicked!");
+
+//              Activity activity = (Activity) mContext;
+//
+//              Intent bind_intent = new Intent();
+//              bind_intent.setClassName(IntentConsts.Sync.APPLICATION_NAME,
+//                      IntentConsts.Sync.SYNC_SERVICE_CLASS);
+//              mContext.bindService(bind_intent, mConnnection,
+//                      Context.BIND_AUTO_CREATE | ((Build.VERSION.SDK_INT >= 14) ?
+//                              Context.BIND_ADJUST_WITH_ACTIVITY :
+//                              0));
+//
+//              ((ISyncServiceInterfaceActivity)activity)
+//                      .invokeSyncInterfaceAction(new DoSyncActionCallback() {
+//                        @Override public void doAction(OdkSyncServiceInterface syncServiceInterface)
+//                                throws RemoteException {
+//                          if ( syncServiceInterface != null ) {
+//                            final SyncStatus status = syncServiceInterface.getSyncStatus(ODKFileUtils.getOdkDefaultAppName());
+//                            //final SyncProgressEvent event = syncServiceInterface.getSyncProgressEvent(getAppName());
+//                            if (status == SyncStatus.SYNCING) {
+//                              syncAction = SyncActions.MONITOR_SYNCING;
+//
+////                              handler.post(new Runnable() {
+////                                @Override
+////                                public void run() {
+////                                  showProgressDialog(status, event.progressState,
+////                                          event.progressMessageText, event.curProgressBar, event.maxProgressBar);
+////                                }
+////                              });
+//                              return;
+//                            }
+//
+//                            switch (syncAction) {
+//                              case SYNC:
+//                                syncServiceInterface.synchronizeWithServer(ODKFileUtils.getOdkDefaultAppName(), syncAttachmentState);
+//                                syncAction = SyncActions.MONITOR_SYNCING;
+//
+////                                handler.post(new Runnable() {
+////                                  @Override
+////                                  public void run() {
+////                                    showProgressDialog(SyncStatus.NONE, null, getString(R.string.sync_starting), -1, 0);
+////                                  }
+////                                });
+//                                break;
+//                              default:
+//                                break;
+//                            }
+//                          } else {
+////                            WebLogger.getLogger(getAppName()).d(TAG, "[" + getId() + "] [tickleInterface] syncServiceInterface == null");
+////                            // The service is not bound yet so now we need to try again
+////                            handler.postDelayed(new Runnable() {
+////                              @Override
+////                              public void run() {
+////                                tickleInterface();
+////                              }
+////                            }, 100);
+//                          }
+//                        }
+//                      });
+
+              //TODO: Skip SyncActivity and invoke service methods directly
+
+              ArrayList<String> ids = new ArrayList<String>();
+              ids.add(info.UUID);
+
+              Map<String, ArrayList<String>> listToSync = new HashMap<>();
+              listToSync.put(info.tableId, ids);
+              Intent syncIntent = new Intent();
+              syncIntent.setComponent(new ComponentName(
+                      IntentConsts.Sync.APPLICATION_NAME,
+                      IntentConsts.Sync.ACTIVITY_NAME));
+              syncIntent.setAction(Intent.ACTION_DEFAULT);
+              Bundle bundle = new Bundle();
+              bundle.putString(IntentConsts.INTENT_KEY_APP_NAME, ODKFileUtils.getOdkDefaultAppName());
+              syncIntent.putExtras(bundle);
+              for (Map.Entry<String, ArrayList<String>> entry : listToSync.entrySet()) {
+                bundle.putSerializable(entry.getKey(), entry.getValue());
+              }
+              syncIntent.putExtras(bundle);
+
+              mContext.startActivity(syncIntent);
             }
           });
 
